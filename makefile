aabee40cf7dfee07cf14eabf43f16e63814861e1
Jason Wang <jason.ruonan.wang@gmail.com>
2021-06-07 16:46:57 -0400

    add tableCopy.cc

---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	mpic++ -std=c++11 tableCopy.cc -o tableCopy -lcasa_casa -lcasa_tables
---
+++ tableCopy.cc
@@ -0,0 +1,82 @@
+#include <casacore/tables/Tables.h>
+#include <casacore/tables/Tables/TableCopy.h>
+#include <casacore/tables/DataMan/Adios2StMan.h>
+
+using namespace casacore;
+using namespace std;
+
+
+std::string sparseColumnName = "map";
+
+//std::string useCompressor = "bzip2";
+std::string useCompressor = "blosc";
+
+bool checkTables(std::string table1, std::string table2)
+{
+    Table t1(table1);
+    Table t2(table2);
+    ArrayColumn<Complex> ac1(t1, sparseColumnName);
+    ArrayColumn<Complex> ac2(t2, sparseColumnName);
+
+    Array<Complex> a1 = ac1.get(0);
+    Array<Complex> a2 = ac2.get(0);
+
+    if(a1.nelements() != a2.nelements())
+    {
+        cerr << "wrong number of elements " << endl;
+    }
+
+    for(size_t j=0; j<a1.nelements(); ++j)
+    {
+        if(a1.data()[j] != a2.data()[j])
+        {
+            cerr << "wrong data at " << j << endl;
+            return false;
+        }
+    }
+
+    cout << "============================== Input Table Structure ==============================" << endl;
+    t1.showStructure(cout);
+    cout << "===================================================================================" << endl << endl;
+
+    cout << "============================== Output Table Structure =============================" << endl;
+    t2.showStructure(cout);
+    cout << "===================================================================================" << endl << endl;
+
+    return true;
+}
+
+
+int main (int argc, const char* argv[])
+{
+    string fileIn, fileOut;
+
+    if(argc<3)
+    {
+        std::cout << "Please provide filenames for input and output table, e.g. run ./tableCopy input.table output.table";
+    }
+    else
+    {
+        fileIn = argv[1];
+        fileOut = argv[2];
+    }
+
+    {
+        Table tabIn(fileIn);
+        ArrayColumn<Complex> mapColumn(tabIn, sparseColumnName);
+        TableDesc td("", "1", TableDesc::Scratch);
+        td.addColumn (ArrayColumnDesc<Complex>(sparseColumnName, mapColumn.shape(0), ColumnDesc::FixedShape));
+        SetupNewTable newtab(fileOut, td, Table::New);
+
+        Adios2StMan a2stman(MPI_COMM_WORLD, "table", {{"Compressor",useCompressor}}, {});
+        newtab.bindAll(a2stman);
+        Table tabOut(newtab);
+        tabOut.addRow(tabIn.nrow());
+        TableCopy::copySubTables(tabOut, tabIn);
+        TableCopy::copyColumnData(tabIn, sparseColumnName, tabOut, sparseColumnName, false);
+    }
+
+    checkTables(fileIn, fileOut);
+
+    return 0;
+}
